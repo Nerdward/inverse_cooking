815088be7bd0cd9ffa1175409f3aee9eaed3404d
Ohakim Nnaemeka <ohakim.nnaemeka@gmail.com>
2022-08-16 18:29:48 +0000

    add a function to image script

--- streamlit/pages/image_to_recipe.py
+++ streamlit/pages/image_to_recipe.py
@@ -2,14 +2,40 @@ import streamlit as st
 import time
 import numpy as np
 import requests
+import torch
+import pickle
 from src.predict import *
 from src import generate
 from pathlib import Path
+from src.model import get_model
 
 BASE_DIR = Path(__file__).parents[2]
-# @st.cache()
+
+def load_artifacts_in(ingr_path, vocab_path, model_path):
+    import sys; sys.argv=['']; del sys
+    args = get_parser()
+    args.maxseqlen = 15
+    args.ingrs_only=False
+    
+    with open(ingr_path, "rb") as f:
+        ingrs_vocab = pickle.load(f)
+    with open(vocab_path, "rb") as f:
+        vocab = pickle.load(f)
+    # ingrs_vocab = pickle.load(open(ingr_path, 'rb'))
+    # vocab = pickle.load(open(vocab_path, 'rb'))
+
+    ingr_vocab_size = len(ingrs_vocab)
+    instrs_vocab_size = len(vocab)
+    model = get_model(args, ingr_vocab_size, instrs_vocab_size)
+    # Load the trained model parameters
+
+    model.load_state_dict(torch.load(model_path, map_location=map_loc))
+
+    return model, ingrs_vocab, vocab
+
+@st.cache()
 def load_app_artifacts():
-    model, ingrs_vocab, vocab = load_artifacts(ingr_path=Path(BASE_DIR, 'app/artifacts/ingr_vocab.pkl'),
+    model, ingrs_vocab, vocab = load_artifacts_in(ingr_path=Path(BASE_DIR, 'app/artifacts/ingr_vocab.pkl'),
     vocab_path=Path(BASE_DIR, 'app/artifacts/instr_vocab.pkl'),
     model_path=Path(BASE_DIR, 'app/artifacts/modelbest.ckpt'))
     
